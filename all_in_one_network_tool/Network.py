459e88936cc91af7cb2857cf982a5dbb44b846d7
Rafik Harzi <harzi.rafik@gmail.com>
2015-10-09 23:43:09 +0200

    adding ping TCP: SYN and Connect scans

--- all_in_one_network_tool/Network.py
+++ all_in_one_network_tool/Network.py
@@ -4,6 +4,16 @@ import netifaces as ni
 import pprint
 import iptools
 
+'''
+Detect the live host on the network (host discovery)
+Detect the open ports on the host (port discovery or enumeration)
+Detect the software and the version to the respective port (service discovery)
+Detect the operating system, hardware address, and the software version
+Detect the vulnerability and security holes (Nmap scripts)
+'''
+
+
+
 class Network:
 	my_addr = ""
 	my_netmask = ""
@@ -20,6 +30,30 @@ class Network:
 					print ("interface %s is connected and has this addresse %s/%s " % (intr, self.my_addr, self.my_netmask))
 				except KeyError:
 					print ("interface %s is not connected !!" % intr)
+
+	def get_hosts(self):
+		""" Ping Scan """
+		nm = nmap.PortScanner()
+		print 
+		print("Scannig the network for live hosts using ping")
+		pp = pprint.PrettyPrinter(indent=2)
+		pp.pprint(nm.scan(hosts=self.my_addr+'/'+str(iptools.ipv4.netmask2prefix(self.my_netmask)), arguments='-sP'))
+
+	def tcp_syn_scan(self):
+		"""TCP SYN Scan"""
+		nm = nmap.PortScanner()
+		print 
+		print("Scannig the network for live hosts using SYN packets ")
+		pp = pprint.PrettyPrinter(indent=2)
+		pp.pprint(nm.scan(hosts=self.my_addr+'/'+str(iptools.ipv4.netmask2prefix(self.my_netmask)), arguments='-sS'))
+
+	def tcp_connect_scan(self):
+		"""TCP CONNECT Scan"""
+		nm = nmap.PortScanner()
+		print 
+		print("Scannig the network for live hosts using Full TCP connection to detect open TCP ports")
+		pp = pprint.PrettyPrinter(indent=2)
+		pp.pprint(nm.scan(hosts=self.my_addr+'/'+str(iptools.ipv4.netmask2prefix(self.my_netmask)), arguments='-sS'))
 
 	def scan_network(self):
 		nm = nmap.PortScanner()
